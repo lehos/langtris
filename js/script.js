6cfa93480004eaaabdef890bf5ad29ad57435105
lehos <alexey.stratan@gmail.com>
2012-07-30 09:48:49 +0400

    js

--- js/script.js
+++ js/script.js
@@ -1,49 +1,149 @@
-var Langtris = function(){
-	var MAP_ROW_COUNT = 12;
-	var MAP_COLUMN_COUNT = 5;
+function random(from, to){
+	return Math.floor(Math.random() * (to - from + 1) + from);
+}
 
-	//TODO: рассчитывать динамически
-	var BRICK_W = 143;
-	var BRICK_H = 41;
+var Langtris = function(){
+	var config = {
+		map_row_count: 12,
+		map_column_count: 5,
+
+		brick_w: 143,
+		brick_h: 41,
+
+		fall_speed: 5000,
+		fall_delay: 4000
+	};
+
+	var langs = ["en", "ru"];
+	var en_dic = ["city", "cucumber", "gloves", "learn", "man", "skin", "spoon", "swim", "razor", "watch"];
+	var ru_dic = ["город", "огурец", "перчатки", "учиться", "мужчина", "кожа", "ложка", "плавать", "бритва", "часы"];
 
 	var $tris = $("#tris");
 	var $brick_template = $("#template-brick");
 
 	var me = this;
 
-	me.map = [];
-	var elems = "";
+	/**
+	 * массив колонок, каждая из которых,
+	 * в свою очередь, массив объектов-кирпичиков
+	 * @type {Array}
+	 */
+	var wall = [];
 
-	//отсчет координат идёт из нижнего левого угла
-	//заполняем массив объектами-кирпичами
-	for (var i = 0; i < MAP_ROW_COUNT; i++){
-		var row_arr = [];
-		for (var j = 0; j < MAP_COLUMN_COUNT; j++){
-			row_arr.push({
-				state: 0, //поле изначально пустое
-				language: "ru",
-				word: "пельмень",
-				row_number: i + 1,
-				column_number: j + 1,
-				left: BRICK_W * j,
-				bottom: BRICK_H * i
-			});
+	var bricks_rain = function(){
+//		выбираем словарь
+		var lang = langs[random(0, 1)];
+		var lang_dic = eval(lang + "_dic");
 
-			elems = elems + $brick_template.render({
-				left: BRICK_W * j,
-				bottom: BRICK_H * i,
-				text: "aas",
-				lang: "ru"
-			});
+//		выбираем слово
+		var word = lang_dic[random(0, lang_dic.length)];
+
+//		выбираю колонку, в которое будет падать слово
+		var column = random(0, config.map_column_count - 1);
+
+//		высчитываю пустое место (номер строки)
+		var row;
+//		если колонка есть (даже пустая)
+		if (wall[column]){
+			row = wall[column].length;
+//		если же колонки нет
+		} else {
+			wall[column] = [];
+			row = 0;
 		}
-		me.map.push(row_arr);
-	}
 
-	$tris.append(elems);
+		var obj = {
+			lang: lang,
+			word: word,
+			row: row,
+			column: column,
+			left: config.brick_w * column,
+			bottom: config.brick_h * row
+		};
 
+		var elem_html = $brick_template.render({
+			lang: lang,
+			word: word,
+			left: config.brick_w * column,
+			bottom: config.brick_h * row
+		});
+
+		wall[column][row]
+
+		console.log(elem_html);
+		
+//		var row = wall[column];
+//		console.log(column, row);
+	}();
+
+//	//карта областей
+//	for (var i = 0; i < config.map_row_count; i++){
+//		var row_arr = [];
+//		for (var j = 0; j < config.map_column_count; j++){
+//			row_arr.push({
+//				lang: "",
+//				word: "",
+//				row_number: i,
+//				column_number: j
+//			});
+//		}
+//		me.map.push(row_arr);
+//	}
+
+	/**
+	 * создание кирпичика
+	 * @param {object} params
+	 * @config {string} [lang]
+	 * @config {string} [word]
+	 * @config {int} [row] row number, 1-based, counts from bottom
+	 * @config {int} [column] column number, 1-based
+	 */
+	var brick_create = function(params){
+		var elem_html = $brick_template.render(params);
+
+		params["html"] = elem_html;
+
+		return params;
+	};
+
+	/**
+	 * кирпичик падает
+	 * @param {object} params
+	 * @config {string} [lang]
+	 * @config {string} [word]
+	 * @config {int} [row] row number, 1-based, counts from bottom
+	 * @config {int} [column] column number, 1-based
+	 */
+	var brick_fall = function(params){
+		var $elem = brick_create(params);
+
+		$tris.append($elem);
+
+
+		$elem.animate({bottom:"0px"}, config.fall_speed);
+	};
+
+
+//
+//	var fall_timeout;
+//
+//	var init = function(){
+//		var params = {
+//
+//		};
+//		var fall = setInterval(brick_fall(params), config.fall_delay);
+//	};
+//
+//	brick_fall({
+//		lang: "en",
+//		word: "city",
+//		row: 0,
+//		column: 1
+//	});
+
+//	$tris.append(elems);
 };
 
 $(document).ready(function(){
 	var langtris = new Langtris();
-//	console.log(langtris);
 });
